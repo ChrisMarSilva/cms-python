3b2da8f9a59401d8a5b2c145436c6d4fd14e0ef6
ChrisMarSilva <chris.mar.silva@gmail.com>
2022-10-06 19:25:57 +0100

    add Teste Web Scraping Scraping Multiple Shopify Stores

---
+++ CMS Teste API e Web/CMS Teste Web Scraping Scraping Multiple Shopify Stores/main.py
@@ -0,0 +1,48 @@
+from loguru import logger
+import datetime as dt
+import time
+import httpx
+import asyncio
+
+
+async def fetch_store(client, url):
+    resp = await client.get(url)
+    products = resp.json()
+    return url, products['products'][0]['title']
+
+
+async def teste_01():
+    with open('stores.txt', 'r') as f:
+        urls = [line.strip() for line in f.readlines()]
+    async with httpx.AsyncClient() as client:
+        tasks = []
+        for url in urls:
+            tasks.append(asyncio.create_task(fetch_store(client, url)))
+        results = await asyncio.gather(*tasks)
+    return results
+
+
+def main():
+    try:
+
+        logger.info(f'Inicio') 
+        start_time = time.perf_counter()
+
+        results = asyncio.run(teste_01())
+        logger.info(results)
+
+        end_time = time.perf_counter() - start_time
+        logger.info(f"Fim - Done in {end_time:.2f}s - {dt.timedelta(seconds=end_time)}")
+
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        logger.error(f'Falha Geral(main): "{str(e)}"')
+
+
+if __name__ == '__main__':
+    main()
+
+# python -m pip install --upgrade httpx
+
+# python main.py
