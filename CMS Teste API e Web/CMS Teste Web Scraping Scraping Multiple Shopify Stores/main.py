772c2b7719d62ab677366361d6b155947c4f4069
ChrisMarSilva <chris.mar.silva@gmail.com>
2022-10-07 14:49:13 +0100

    Update main.py

--- CMS Teste API e Web/CMS Teste Web Scraping Scraping Multiple Shopify Stores/main.py
+++ CMS Teste API e Web/CMS Teste Web Scraping Scraping Multiple Shopify Stores/main.py
@@ -1,18 +1,19 @@
-from loguru import logger
+import asyncio
 import datetime as dt
 import time
+
 import httpx
-import asyncio
+from loguru import logger
 
 
 async def fetch_store(client, url):
     resp = await client.get(url)
     products = resp.json()
-    return url, products['products'][0]['title']
+    return url, products["products"][0]["title"]
 
 
 async def teste_01():
-    with open('stores.txt', 'r') as f:
+    with open("stores.txt", "r") as f:
         urls = [line.strip() for line in f.readlines()]
     async with httpx.AsyncClient() as client:
         tasks = []
@@ -25,7 +26,7 @@ async def teste_01():
 def main():
     try:
 
-        logger.info(f'Inicio') 
+        logger.info(f"Inicio")
         start_time = time.perf_counter()
 
         results = asyncio.run(teste_01())
@@ -40,9 +41,21 @@ def main():
         logger.error(f'Falha Geral(main): "{str(e)}"')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 # python -m pip install --upgrade httpx
+# python -m pip install --upgrade flake8
+# python -m pip install --upgrade black
+# python -m pip install --upgrade mypy
+# python -m pip install --upgrade pylint
+# python -m pip install --upgrade bandit
+# python -m pip install --upgrade pydocstyle
+
 
 # python main.py
+# python -m black main.py
+# python -m mypy main.py
+# python -m pylint main.py
+# python -m bandit main.py
+# python -m pydocstyle test_main.py
