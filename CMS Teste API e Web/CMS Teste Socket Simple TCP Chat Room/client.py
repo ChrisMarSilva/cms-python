039116d110403a8b771a5d72111c9aa4c06b1c21
ChrisMarSilva <chris.mar.silva@gmail.com>
2022-10-14 17:51:31 +0100

    add tests with Socket Simple TCP Chat Room

--- CMS Teste API e Web/CMS Teste Socket Simple TCP Chat Room/client.py
+++ CMS Teste API e Web/CMS Teste Socket Simple TCP Chat Room/client.py
@@ -1,17 +1,50 @@
-import datetime as dt
 import socket
-import time
+import threading
+
+# https://www.youtube.com/watch?v=3UOyky9sEQY
+# https://www.neuralnine.com/tcp-chat-in-python/
 
 
 def main():
+
+    host = "127.0.0.1"  # "localhost"
+    port = 5000  # 55555
+
+    # Choosing Nickname
+    nickname = input("Choose your nickname: ")
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect(("localhost", 5000))
+        client.connect((host, port))
 
-        data = f"DtHr: {dt.datetime.now()}"
-        client.send(data.encode("utf-8"))
+        # Listening to Server and Sending Nickname
+        def receive():
+            while True:
+                try:
+                    # Receive Message From Server
+                    # If 'NICK' Send Nickname
+                    message = client.recv(1024).decode()  # "ascii"
+                    if message == "NICK":
+                        client.send(nickname.encode())  # "ascii"
+                    else:
+                        print(message)
+                except:
+                    # Close Connection When Error
+                    print("An error occured!")
+                    client.close()
+                    break
 
-        data = client.recv(1024)
-        print(data.decode("utf-8"))
+        # Sending Messages To Server
+        def write():
+            while True:
+                message = "{}: {}".format(nickname, input(""))
+                client.send(message.encode())  # "ascii"
+
+        # Starting Threads For Listening And Writing
+        receive_thread = threading.Thread(target=receive)
+        receive_thread.start()
+
+        write_thread = threading.Thread(target=write)
+        write_thread.start()
 
 
 if __name__ == "__main__":
--- CMS Teste API e Web/CMS Teste Socket Simple TCP Chat Room/server.py
+++ CMS Teste API e Web/CMS Teste Socket Simple TCP Chat Room/server.py
@@ -1,26 +1,68 @@
 import socket
+import threading
+
+# https://www.youtube.com/watch?v=3UOyky9sEQY
+# https://www.neuralnine.com/tcp-chat-in-python/
 
 
 def main():
+
+    host = "127.0.0.1"  # "localhost"
+    port = 5000  # 55555
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.bind(("localhost", 5000))
-        server.listen(5)
+        server.bind((host, port))
+        server.listen()
 
-        while True:
-            conn, addr = server.accept()
-            print(f"Connected to {addr}")
+        # Lists For Clients and Their Nicknames
+        clients = []
+        nicknames = []
 
-            if conn:
-                data = conn.recv(1024)
-                if not data:
+        # Sending Messages To All Connected Clients
+        def broadcast(message):
+            for client in clients:
+                client.send(str(message).encode())
+
+        # Handling Messages From Clients
+        def handle(client):
+            while True:
+                try:
+                    # Broadcasting Messages
+                    message = client.recv(1024)
+                    broadcast(message)
+                except:
+                    # Removing And Closing Clients
+                    index = clients.index(client)
+                    clients.remove(client)
+                    client.close()
+                    nickname = nicknames[index]
+                    broadcast("{} left!".format(nickname).encode())  # "ascii"
+                    nicknames.remove(nickname)
                     break
-                if len(str(data)) <= 0:
-                    break
 
-                print(data.decode("utf-8"))
+        # Receiving / Listening Function
+        def receive():
+            while True:
+                # Accept Connection
+                client, address = server.accept()
+                print("Connected with {}".format(str(address)))
 
-                data = "Got yout message! Thank you!"
-                conn.send(data.encode("utf-8"))
+                # Request And Store Nickname
+                client.send("NICK".encode())  # "ascii"
+                nickname = client.recv(1024).decode()  # "ascii"
+                nicknames.append(nickname)
+                clients.append(client)
+
+                # Print And Broadcast Nickname
+                print("Nickname is {}".format(nickname))
+                broadcast("{} joined!".format(nickname).encode())  # "ascii"
+                client.send("Connected to server!".encode())  # "ascii"
+
+                # Start Handling Thread For Client
+                thread = threading.Thread(target=handle, args=(client,))
+                thread.start()
+
+        receive()
 
 
 if __name__ == "__main__":
